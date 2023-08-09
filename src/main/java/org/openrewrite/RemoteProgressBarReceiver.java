83673a9b6819bbd43c643ffd7ed0aca8e42392d1
Jonathan Schneider <jkschneider@gmail.com>
2023-08-09 10:28:02 -0400

    OmniParser and ProgressBar

---
+++ src/main/java/org/openrewrite/RemoteProgressBarReceiver.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite;
+
+import io.moderne.commons.ui.RemoteProgressBarSender.Request.Type;
+import org.openrewrite.internal.lang.Nullable;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static io.moderne.commons.ui.RemoteProgressBarSender.MAX_MESSAGE_SIZE;
+import static java.util.Objects.requireNonNull;
+
+public class RemoteProgressBarReceiver implements ProgressBar {
+    private final ProgressBar delegate;
+
+    private final DatagramSocket socket;
+
+    public RemoteProgressBarReceiver(ProgressBar delegate) {
+        this.delegate = delegate;
+        try {
+            this.socket = new DatagramSocket(RemoteProgressBarSender.PORT);
+        } catch (SocketException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    public void run() {
+        try {
+            byte[] buf = new byte[MAX_MESSAGE_SIZE]; // no message should be longer than a terminal line length
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+
+            Type type = null;
+            for (Type t : Type.values()) {
+                if (t.ordinal() == buf[0]) {
+                    type = t;
+                    break;
+                }
+            }
+
+            if (type == null) {
+                return;
+            }
+
+            String message = null;
+            if (packet.getLength() > 1) {
+                message = new String(Arrays.copyOfRange(buf, 1, packet.getLength()),
+                        StandardCharsets.UTF_8);
+            }
+
+            switch (type) {
+                case IntermediateResult:
+                    delegate.intermediateResult(message);
+                    break;
+                case Finish:
+                    delegate.finish(message);
+                    break;
+                case Close:
+                    delegate.close();
+                    break;
+                case Step:
+                    delegate.step();
+                    break;
+                case SetExtraMessage:
+                    delegate.setExtraMessage(message);
+                    break;
+                case SetMax:
+                    delegate.setMax(Integer.parseInt(requireNonNull(message)));
+                    break;
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @Override
+    public void intermediateResult(@Nullable String message) {
+        delegate.intermediateResult(message);
+    }
+
+    @Override
+    public void finish(String message) {
+        delegate.finish(message);
+    }
+
+    @Override
+    public void step() {
+        delegate.step();
+    }
+
+    @Override
+    public ProgressBar setExtraMessage(String extraMessage) {
+        return delegate.setExtraMessage(extraMessage);
+    }
+
+    @Override
+    public ProgressBar setMax(int max) {
+        return delegate.setMax(max);
+    }
+
+    @Override
+    public void close() {
+    }
+}
---
+++ src/main/java/org/openrewrite/RemoteProgressBarSender.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite;
+
+import lombok.Value;
+import org.openrewrite.internal.lang.Nullable;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class RemoteProgressBarSender implements ProgressBar {
+    final static int PORT = 37245; // moderne blue % 65535 :)
+    final static int MAX_MESSAGE_SIZE = 256;
+
+    private final DatagramSocket socket;
+    private final InetAddress address;
+
+    public RemoteProgressBarSender() {
+        this(null);
+    }
+
+    public RemoteProgressBarSender(@Nullable InetAddress address) {
+        try {
+            String localhost = Files.exists(Paths.get("/.dockerenv")) ? "host.docker.internal" : "localhost";
+            this.address = address == null ? InetAddress.getByName(localhost) : address;
+            this.socket = new DatagramSocket();
+        } catch (UnknownHostException | SocketException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @Override
+    public void intermediateResult(@Nullable String message) {
+        send(Request.Type.IntermediateResult, message);
+    }
+
+    @Override
+    public void finish(String message) {
+        send(Request.Type.Finish, message);
+    }
+
+    @Override
+    public void close() {
+        send(Request.Type.Close, null);
+    }
+
+    @Override
+    public void step() {
+        send(Request.Type.Step, null);
+    }
+
+    @Override
+    public ProgressBar setExtraMessage(String extraMessage) {
+        send(Request.Type.SetExtraMessage, extraMessage);
+        return this;
+    }
+
+    @Override
+    public ProgressBar setMax(int max) {
+        send(Request.Type.SetMax, null);
+        return this;
+    }
+
+    private void send(Request.Type type, @Nullable String message) {
+        try {
+            if (message != null && message.length() + 1 > MAX_MESSAGE_SIZE) {
+                throw new IllegalArgumentException("Message size exceeded maximum length: " + message);
+            }
+            byte[] buf = (type.ordinal() + (message == null ? "" : message)).getBytes();
+            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, PORT);
+            socket.send(packet);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    @Value
+    static class Request {
+        enum Type {
+            IntermediateResult,
+            Finish,
+            Close,
+            Step,
+            SetExtraMessage,
+            SetMax
+        }
+
+        Type type;
+
+        @Nullable
+        String body;
+    }
+}
