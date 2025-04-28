a38907a352577201a6c67b6bd1f5a09da798710b
Kevin Carpenter™️ <kevin@moderne.io>
2025-04-28 12:58:14 -0300

    Remove dependency on JavaScript (#32)
    As OpenRewrite has decided that all new modules will be moved inside openrewrite/rewrite,
    we now have decided to exclude rewrite-javascript from rewrite-all.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation("org.openrewrite:rewrite-yaml")
     implementation("org.openrewrite:rewrite-python:$latest")
     implementation("org.openrewrite:rewrite-csharp:$latest")
-    implementation("org.openrewrite:rewrite-javascript:$latest")
 
     testImplementation("org.openrewrite:rewrite-test")
     testRuntimeOnly("org.openrewrite:rewrite-java-17")
